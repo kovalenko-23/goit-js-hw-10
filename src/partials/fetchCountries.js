935bdfbd91ed1f96ba5f02b5c80caa6fcc51b9b2
Vitalii Kovalenko <vitaliikovalenko23@gmail.com>
2021-07-22 22:58:12 +0300

    Update fetchCountries.js

--- src/partials/fetchCountries.js
+++ src/partials/fetchCountries.js
@@ -1,10 +1,9 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
 
-function fetchCountry(name) {
-    return fetch(`${BASE_URL}${name}?fields=name;capital;flag;population;languages`)
-        .then(response => {
-            return response.json();
-        })
+async function fetchCountry(name) {
+    const responce = await fetch(`${BASE_URL}${name}?fields=name;capital;flag;population;languages`)
+    const country = await responce.json();
+    return country;       
 };
 
 export default {fetchCountry}
